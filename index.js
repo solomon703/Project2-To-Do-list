984c34e447f01adbf4d42ba892c74c3515181a2c
rajuboyella1753 <rajuboyella737@gmail.com>
2024-07-08 11:22:31 +0530

    Checkout2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const listContainer = document.getElementById("list-container");
 
 function addTask(){
   if(inputBox.value === ''){
-    alert("You must write something");
+    alert("You Must Have To Write Something.");
   }
   else{
     let li = document.createElement("li");
